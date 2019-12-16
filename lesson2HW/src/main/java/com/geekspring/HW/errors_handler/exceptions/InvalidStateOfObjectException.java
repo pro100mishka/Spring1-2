5e9982d32e550da117513608ce5ad7d0e7c05f73
Pro100mishka <pro100gnomik@gmail.com>
2019-12-16 19:44:26 +0600

    ToPullRequest

---
+++ lesson2HW/src/main/java/com/geekspring/HW/errors_handler/exceptions/InvalidStateOfObjectException.java
@@ -0,0 +1,7 @@
+package com.geekspring.HW.errors_handler.exceptions;
+
+public class InvalidStateOfObjectException extends RuntimeException {
+    public InvalidStateOfObjectException(String message) {
+        super(message);
+    }
+}
